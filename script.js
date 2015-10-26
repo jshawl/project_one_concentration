e1b7be81d702b367c2abe0a8fe44bc3dffb10bdf
Thom Householder <thomhouseholder@gmail.com>
2015-10-26 10:17:32 -0400

    pseudo code layout

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ $('.card').on('click', flipCard)
 
 function flipCard(evt){
   console.log('card clicked')
+  //add toggle from white to color for indiv cards
 }
+
+//assign colors to cards
+
+
+
+//reset button
