7f73dc66dbaaff88c01d63de961b7f0c3e51a86e
Thom Householder <thomhouseholder@gmail.com>
2015-10-26 11:31:09 -0400

    added colors to cards

--- script.js
+++ script.js
@@ -7,7 +7,26 @@ function flipCard(evt){
 }
 
 //assign colors to cards
-
+  //rowOne
+$('div.rowOne .card:eq(0)').css('background', 'blue')
+$('div.rowOne .card:eq(1)').css('background', 'red')
+$('div.rowOne .card:eq(2)').css('background', 'green')
+$('div.rowOne .card:eq(3)').css('background', 'orange')
+  //rowTwo
+$('div.rowTwo .card:eq(0)').css('background', 'blue')
+$('div.rowTwo .card:eq(1)').css('background', 'lime')
+$('div.rowTwo .card:eq(2)').css('background', 'olive')
+$('div.rowTwo .card:eq(3)').css('background', 'brown')
+  //rowThree
+$('div.rowThree .card:eq(0)').css('background', 'tan')
+$('div.rowThree .card:eq(1)').css('background', 'orange')
+$('div.rowThree .card:eq(2)').css('background', 'tan')
+$('div.rowThree .card:eq(3)').css('background', 'red')
+  //rowFour
+$('div.rowFour .card:eq(0)').css('background', 'lime')
+$('div.rowFour .card:eq(1)').css('background', 'green')
+$('div.rowFour .card:eq(2)').css('background', 'brown')
+$('div.rowFour .card:eq(3)').css('background', 'olive')
 
 
 //reset button
