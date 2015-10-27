bc5812b87d9481d802e3a30c178aad7c27adf5d1
Thom Householder <thomhouseholder@gmail.com>
2015-10-27 09:45:12 -0400

    color selection works with two classes, one div

--- script.js
+++ script.js
@@ -3,39 +3,23 @@ $('.card').on('click', flipCard)
 
 function flipCard(evt){
   console.log('card clicked')
+  // var click1=$(this).css('class')
+  // var click2
   //add toggle from white to color for indiv cards
-  $(this).toggle('blue')
-}
-//
-// function changeColor(){
-//   $('#grid').children().eq(0).removeClass('blue')
-// }
-//
-// var conCards=
-//     [$('#grid').children().eq(0).css('background-color','blue'),
-//      $('#grid').children().eq(1).addClass('red'),
-//      $('#grid').children().eq(2).addClass('green'),
-//      $('#grid').children().eq(3).addClass('orange'),
-//      $('#grid').children().eq(4).addClass('blue'),
-//      $('#grid').children().eq(5).addClass('lime'),
-//      $('#grid').children().eq(6).addClass('olive'),
-//      $('#grid').children().eq(7).addClass('brown'),
-//      $('#grid').children().eq(8).addClass('tan'),
-//      $('#grid').children().eq(9).addClass('orange'),
-//      $('#grid').children().eq(10).addClass('tan'),
-//      $('#grid').children().eq(11).addClass('red'),
-//      $('#grid').children().eq(12).addClass('lime'),
-//      $('#grid').children().eq(13).addClass('green'),
-//      $('#grid').children().eq(14).addClass('brown'),
-//      $('#grid').children().eq(15).addClass('olive'),]
+  $(this).toggleClass('card')
 
-// function cardColor(){
-//   for(i=0; i<conCards.length; i++) {
-//     console.log(conCards[i])
-//   }
-//   }
+}
 
 //compare two clicked cards. if background-colors are equal, alert nice, next move.  if not equal, alert try again and change both back to white
+// (class1===blue) &&  (class2==blue)
+//   alert(good match, pick another pair)
+//
+//   else {
+//     alert(lousy memory, try agin!)
+//     toggle cards back to white
+//   }
+//
+
 
 //how to keep matched pairs from being toggled?
 
