c4dc830a294735022908efc78f302ffe95760f9c
Thom Householder <thomhouseholder@gmail.com>
2015-10-27 17:41:01 -0400

    passed class value to var answer; first step for comparing

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
+
+var oldAnswerGlobal = null
+
 //card clickEvent
 $('.card').on('click', flipCard)
 
 function flipCard(evt){
-  console.log('card clicked')
-  // var click1=$(this).css('class')
-  // var click2
+    console.log('card clicked')
+  $(this).toggleClass('card')
+    console.log(this.className)
+  var answer = this.className
+  oldAnswerGlobal=null
+    console.log('var answer= '+ answer)
+    console.log('oldAnswerGlobal= '+ oldAnswerGlobal)
+  if(oldAnswerGlobal !== null){
+    if(answer == oldAnswerGlobal){
+      alert("Nice match!")
+    }else{
+      alert("Not a match, try again.")
+    }
+
+
   //add toggle from white to color for indiv cards
   $(this).toggleClass('card')
-
+}
 }
 
-//compare two clicked cards. if background-colors are equal, alert nice, next move.  if not equal, alert try again and change both back to white
-// (class1===blue) &&  (class2==blue)
-//   alert(good match, pick another pair)
-//
-//   else {
-//     alert(lousy memory, try agin!)
-//     toggle cards back to white
-//   }
-//
+//compare two clicked cards. if background-colors are equal, alert('nice, next move.')  if not equal, alert('try again.') and change both back to white
+// if ((class of firstCardClicked)==(class of secondCardClicked)){
+//   alert('Nice match!')
+//   // and 'deactivate' cards
+// } else {
+//   alert('Not a match. Try again.')
+//   // and add .card back to both cards.
+//   $('#grid').children(/*whatselector?*/).addClass('card')
+// }
 
-
-//how to keep matched pairs from being toggled?
-
-//alert 'winner, game over' when 8 pairs have been made
 
 //reset button to restart game
 $('button').on('click', gameReset)
 
 function gameReset(evt){
   $('#grid').children().addClass('card')
-  alert('Playing again, eh?')
+  alert('Playing again? Good luck!')
 }
 
 //bonus: shuffle colors on reset
